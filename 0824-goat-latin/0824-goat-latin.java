08c170210f74977cde69777c63235e333566f544
Madhumitha1124 <madhumithasakthivel188@gmail.com>
2026-08-31 20:30:49 +0530

    Time: 2 ms (82.9%), Space: 43.1 MB (73.22%) - LeetHub

--- 0824-goat-latin/0824-goat-latin.java
+++ 0824-goat-latin/0824-goat-latin.java
@@ -1,26 +1,23 @@
 class Solution {
     public boolean isvowel(char c){
-        return "aeiouAEIOU".indexOf(c) != -1;
+      return "aeiouAEIOU".indexOf(c)!=-1;
     }
-
     public String toGoatLatin(String sentence) {
-        StringBuilder ans = new StringBuilder();
-        String[] words = sentence.split(" ");
-
-        for(int i=0; i<words.length; i++){
-            String w = words[i];
-
+        StringBuilder ans=new StringBuilder();
+        String[]word=sentence.split(" ");
+        for(int i=0;i<word.length;i++){
+            String w=word[i];
             if(isvowel(w.charAt(0))){
                 ans.append(w);
-            } else {
+            }
+            else{
                 ans.append(w.substring(1)).append(w.charAt(0));
             }
-
             ans.append("ma");
             ans.append("a".repeat(i+1));
-
-            if(i < words.length-1) ans.append(" ");
+            if(i<word.length-1) ans.append(" ");
         }
         return ans.toString();
+        
     }
 }
